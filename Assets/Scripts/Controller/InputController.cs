568c47aa4a22ae52e18dcb631313f7fa990cbdec
문주원 <167047504+S014RMoonJuWon@users.noreply.github.com>
2024-05-16 17:34:44 +0900

    PaddleBug
    패들 버그 확인

---
+++ Assets/Scripts/Controller/InputController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class InputController : PaddleController
+{
+    public void OnMove(InputValue value)
+    {
+        Vector2 moveInput = value.Get<Vector2>().normalized;
+
+        CallMoveEvent(moveInput);
+    }
+}
