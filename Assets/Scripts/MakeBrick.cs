4ed036beb0c1aef6138636d9353facb5ab40abd7
문주원 <167047504+S014RMoonJuWon@users.noreply.github.com>
2024-05-17 12:02:59 +0900

    After Merge
    머지 후 저장

--- Assets/Scripts/MakeBrick.cs
+++ Assets/Scripts/MakeBrick.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MackBrick : MonoBehaviour
+public class MakeBrick : MonoBehaviour
 {
     public GameObject brick;
     void Start()
