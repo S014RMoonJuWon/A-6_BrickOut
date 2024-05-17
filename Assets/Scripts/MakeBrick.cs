200534a00e4c21c59b5faeb0ef8c5d6eb132cf86
attiya1125 <leonrey1125@gmail.com>
2024-05-17 12:01:36 +0900

    Fonts_
    폰트제작

--- Assets/Scripts/MakeBrick.cs
+++ Assets/Scripts/MakeBrick.cs
@@ -2,14 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MackBrick : MonoBehaviour
+public class MakeBrick : MonoBehaviour
 {
+    public GameObject panel;
     public GameObject brick;
-    void Start()
+    public Camera camera;
+    public void ChooseLevel()
     {
-        for (int i = 0; i < 33; i++) 
+        panel.SetActive(false);
+        camera.orthographicSize = 10;
+        for (int i = 0; i < 75; i++) 
         {
             GameObject makeBrick = Instantiate(brick, this .transform);
+
+            float x = (i % 15) * 1.4f - 10f;
+            float y = (i / 15) * 1.3f;
+
+            makeBrick.transform.position = new Vector2(x, y);
+        }
+    }
+
+   public  void ChooseLevel2()
+    {
+        panel.SetActive(false);
+        camera.orthographicSize = 6;
+        for (int i = 0; i < 33; i++)
+        {
+            GameObject makeBrick = Instantiate(brick, this.transform);
 
             float x = (i % 11) * 1.4f - 7f;
             float y = (i / 11) * 1.3f;
@@ -17,7 +36,6 @@ public class MackBrick : MonoBehaviour
             makeBrick.transform.position = new Vector2(x, y);
         }
     }
-
     // Update is called once per frame
     void Update()
     {
