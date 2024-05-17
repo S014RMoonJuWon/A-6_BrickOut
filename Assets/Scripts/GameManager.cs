60081b5b690795f05fd24ce665553999d6526800
dhrzld <dhrzld@gmail.com>
2024-05-17 19:44:47 +0900

    startscene
    스타트씬

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,25 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
 
+    public GameObject gameOverPanel;
+
     private void Awake()
     {
-        if (Instance != null)
+        if (Instance == null)
         {
             Instance = this;
         }
     }
-    void Start()
+    public void GameOver()
     {
-        
+        gameOverPanel.SetActive(true);
     }
-
-    void Update()
+    public void LoadMainMenu()
     {
-
+        string mainMenuSceneName = "StartScene";
+        Debug.Log("Loading Main Menu Scene: " + mainMenuSceneName);
+        SceneManager.LoadScene(mainMenuSceneName);
+    }
+    public void OnMainMenuButtonClick()
+    {
+        LoadMainMenu();
     }
 }
