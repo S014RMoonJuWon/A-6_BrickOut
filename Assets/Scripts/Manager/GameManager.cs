b6979aac5eaa7935d385a313e98e3c105d536622
문주원 <167047504+S014RMoonJuWon@users.noreply.github.com>
2024-05-20 12:05:36 +0900

    HighScore
    게임매니저 내에서 score 및 highscore 관리

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,13 +12,32 @@ public class GameManager : MonoBehaviour
 
     public GameObject gameOverPanel;
 
+    [SerializeField] private TMP_Text currentScoreTxt;
+    [SerializeField] private TMP_Text highScoreTxt;
+
+    private int currentScore = 0;
+    private int highScore = 0;
+    private string hs = "HighScore";
+
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
+            DontDestroyOnLoad(this.gameObject);
         }
     }
+
+    private void Start()
+    {
+        UpdateTextUI();
+    }
+
+    private void Update()
+    {
+        SetHighScore();
+    }
+
     public void GameOver()
     {
         gameOverPanel.SetActive(true);
@@ -30,5 +51,42 @@ public class GameManager : MonoBehaviour
     public void OnMainMenuButtonClick()
     {
         LoadMainMenu();
+    }
+
+    public void AddScore(int score)
+    {
+        currentScore += score;
+        if (currentScore > highScore)
+        {
+            highScore = currentScore;
+        }
+        UpdateTextUI();
+    }
+
+    private void UpdateTextUI()
+    {
+        currentScoreTxt.text = $"Score : {currentScore}";
+        highScoreTxt.text = $"High Score : {highScore}";
+    }
+
+    private void SetHighScore()
+    {
+        if (PlayerPrefs.HasKey(hs))
+        {
+            float highScore = PlayerPrefs.GetFloat(hs);
+            if (highScore < currentScore)
+            {
+                PlayerPrefs.SetFloat(hs, currentScore);
+
+            }
+            else
+            {
+                return;
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetFloat(hs, currentScore);
+        }
     }
 }
